13ff283bd45303fd2215588b0d3462d10633e352
Saurabh Biware <saurabhbiware@Saurabhs-Air.fritz.box>
2021-01-29 18:07:39 +0100

    Added All the components and app ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { ChatEngine } from 'react-chat-engine';
-import { ChatFeed } from './components/ChatFeed';
+import  ChatFeed  from './components/ChatFeed';
+import LoginForm from './components/LoginForm' 
 
 import './App.css';
 
-const App = () => {
-    return(
-        <ChatEngine
-            height= "100vh"
-            projectID= "7dd04ff9-217d-4c44-b0fa-e588fd66548c"
-            userName= "Saurabh"
-            userSecret= "123123"
-            renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps} />}
-        />
-    )
-}
 
+const projectID = '7dd04ff9-217d-4c44-b0fa-e588fd66548c'
+
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+  
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
+    );
+  };
+  
 export default App;
